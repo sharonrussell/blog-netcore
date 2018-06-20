ca4d8ca6cc0bfded2c9470db2602e6f163b679b1
Sharon Russell <sharon.russell@comparethemarket.com>
2018-06-20 14:47:13 +0100

    Skeleton of app

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "This is a dummy blog entry";
+        }
+    }
+}
